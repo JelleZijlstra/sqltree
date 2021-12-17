0dda86045353d9880a987984f6f485d2f90205d8
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-12-16 22:01:24 -0800

    actually run ci (#17)
    It was suspiciously fast

--- tests/test_fixit.py
+++ tests/test_fixit.py
@@ -1,5 +1,7 @@
 from fixit import add_lint_rule_tests_to_module
 
-from sqltree.fixit import SqlFormatRule
+# fixit injects this class again here under a different name,
+# which confuses pyanalyze
+from sqltree.fixit import SqlFormatRule as _SQLFormatRule
 
-add_lint_rule_tests_to_module(globals(), rules=[SqlFormatRule])
+add_lint_rule_tests_to_module(globals(), rules={_SQLFormatRule})
