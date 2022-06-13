a68d5044920dd5bdc7c0d305cfcc020c1127100a
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-06-13 13:17:18 -0700

    fix formatter

--- sqltree/formatter.py
+++ sqltree/formatter.py
@@ -627,6 +627,9 @@ class Formatter(Visitor[None]):
             if index_hint.trailing_comma is not None:
                 self.visit(index_hint.trailing_comma)
 
+    def visit_SimpleTableName(self, node: p.SimpleTableName) -> None:
+        self.visit(node.identifier)
+
     def visit_SubQueryFactor(self, node: p.SubqueryFactor) -> None:
         self.maybe_visit(node.lateral_kw, add_space=True)
         self.visit(node.table_subquery)
